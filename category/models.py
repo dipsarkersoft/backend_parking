2f47517206ba54dc4cc61fbf7c91446f7a546a16
dip_sarker <sarkerdip005@gmail.com>
2025-02-01 19:56:01 +0600

    categorymodel

--- category/models.py
+++ category/models.py
@@ -7,6 +7,13 @@ class CategoryModel(models.Model):
     price_p_h=models.IntegerField()
     total_slots=models.IntegerField()
     available_slots=models.IntegerField()
+    available_slots_list = models.JSONField(default=list)
+    def save(self, *args, **kwargs):
+        
+        if not self.pk: 
+            self.available_slots_list = {str(i): "free" for i in range(1, self.total_slots + 1)}
+            self.available_slots = self.total_slots
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} - {self.total_slots} -{self.available_slots}"
