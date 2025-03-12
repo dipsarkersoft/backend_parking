c40510bf6a3a32a59c15bea3e828dcdadba8101f
dip_sarker <sarkerdip005@gmail.com>
2025-03-12 12:45:29 +0600

    ssssss

--- Parking/models.py
+++ Parking/models.py
@@ -8,7 +8,7 @@ class ParkingModels(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE )
     category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
     slot = models.ForeignKey(SlotModel, on_delete=models.CASCADE)
-    ticket = models.CharField(max_length=20, unique=True)
+    ticket = models.CharField(max_length=60, unique=True)
     start_park = models.DateTimeField(blank=True, null=True)
     end_park = models.DateTimeField(blank=True, null=True)
     total_price = models.IntegerField(default=0)
